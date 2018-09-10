8d9a614933bf21869da014327a3a709b61396a7b
ren <18435175817@163.com>
2018-09-10 17:40:29 +0800

    wsgi

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-﻿#! /env python
+#! /env python
 # _*_ coding:utf8 _*_
 # @author:ren
 # @date:2018/7/18.11:09
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
 ﻿#! /env python
 # _*_ coding:utf8 _*_
-# @author:ren
-# @date:2018/7/18.11:09
 
 from run import app
 
